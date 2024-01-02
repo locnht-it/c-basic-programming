f32f4eed46891d0f4fc49278880f03d598026100
locnht-it <144313315+locnht-it@users.noreply.github.com>
2024-01-02 20:21:01 +0700

    Create session 01 - Hello
    This is the first project, with the purpose of getting familiar with the C programming language and Dev-C++ (Integrated Development Environment).

---
+++ Code/Session00-Hello/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	printf("Welcome to C world.\n");
+	printf("C is so fun and easy. I quit today!!!");
+	system("pause");
+	
+	return 0;
+}
